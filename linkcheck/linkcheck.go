cc23b5b68426fad1b3f91983c59fd59fcc3f8a40
Carl Johnson <me@carlmjohnson.net>
2021-03-22 11:46:05 -0400

    User agent: upgrade Chrome

--- linkcheck/linkcheck.go
+++ linkcheck/linkcheck.go
@@ -42,7 +42,7 @@ var (
 )
 
 const (
-	chromeUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+	chromeUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
 )
 
 // CLI runs the linkrot executable, equivalent to calling it on the command line.
